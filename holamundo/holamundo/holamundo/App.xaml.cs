d05e741576e83c55e6cabb8df9102af69b51d8ae
FernandoVicente <liammayne12@gmail.com>
2021-02-15 22:36:02 -0400

    Agregando el proyecto basico

---
+++ holamundo/holamundo/holamundo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace holamundo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //  sera la pagina de inicio
+            MainPage = new Page1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ holamundo/holamundo/holamundo/Page1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace holamundo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            /*Content = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Start,
+                Text = "Primer App"
+            };*/
+           
+        }
+        //este se creo con el clicked automatizado de windows forms
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Titulo","Hola mundo","Cancelar","Aceptar");
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //slid.Text = String.Format("Numero: {0:F2}",e.NewValue);
+        }
+    }
+}
